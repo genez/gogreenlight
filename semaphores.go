1892f2c1bb0d42e6c20bbe35cfb7081103479280
Genesio Lini <lini.genesio@gmail.com>
2017-03-14 10:29:36 +0100

    maps was not protected against concurrent [mis]use

--- semaphores.go
+++ semaphores.go
@@ -3,21 +3,30 @@ package gogreenlight
 import (
 	"errors"
 	"fmt"
+	"sync"
 )
 
 type SemaphoreCollection map[string]*namedSemaphore
 
+var mutex sync.RWMutex
 var Semaphores SemaphoreCollection
 
 func init() {
 	Semaphores = SemaphoreCollection{}
+	mutex = sync.RWMutex{}
 }
 
 func (sc *SemaphoreCollection) Add(semaphore *namedSemaphore) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	Semaphores[semaphore.name] = semaphore
 }
 
 func (sc *SemaphoreCollection) Get(name string) (*namedSemaphore, error) {
+	mutex.RLock()
+	defer mutex.RUnlock()
+
 	semaphore, ok := Semaphores[name]
 	if !ok {
 		return nil, errors.New(fmt.Sprintf("Named samaphore %s not found in collection", name))
